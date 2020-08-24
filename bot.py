3e3a3694b068f2662bd3b8224e33da9be26ff25d
kristal374 <70152861+kristal374@users.noreply.github.com>
2020-08-24 21:43:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -569,4 +569,4 @@ async def on_ready():
     channel = client.get_channel(int(733230754817114166))
     await channel.send('К серверу присоеденился {0}!'.format(client.user.name))
 tokin = os.environ.get('bot_token')
-client.run(str(tokin))
+client.run("NzMzMjM3NDQ1MTcyMzk2MDQ0.XxAO6A.KpaRmg3UcZ1rfmEW2VIcwD3YA_E")
